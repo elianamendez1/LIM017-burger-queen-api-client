696c843d56491addf435e44da0e6108146e213e5
BrithBarboza <brith.19.04@gmail.com>
2022-06-14 18:40:14 -0500

    add boilerplate

--- foodelicious/src/app/app.component.ts
+++ foodelicious/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'foodelicious';
+  name: string = 'Bri';
+  age: number = 26;
+  img: string = '/assets/images/background_login.jpg';
+  btnDisabled = false;
+  person = {
+    name: 'Eliana',
+    age: 27,
+    avatar: '/assets/images/background_login.jpg',
+    password: '1234'
+  }
+
 }
+
